c1efdf1b4d7789446a4f0ca57c7b2383887ca98d
vespacarter <skasez@gmail.com>
2015-12-05 16:46:11 +0100

    creates new contact - iteration 3 completed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,20 @@ class ContactsController < ApplicationController
 	def index
 		@contacts = Contact.all
 	end
+
+	def new
+
+	end
+
+	def create
+		contact = Contact.new(
+     		:name => params[:contact][:name],
+     		:address => params[:contact][:address],
+     		:phone => params[:contact][:phone],
+     		:email => params[:contact][:email]
+     	)
+    # Now we save the contact
+    contact.save
+    redirect_to("/contacts")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 # verbo 'ruta' => 'controlador#accion'
+get '/' => 'contacts#index'
  get '/contacts' => 'contacts#index'
+ get '/contacts/new' => 'contacts#new'
+ post '/contacts' => 'contacts#create'
 end
